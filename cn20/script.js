382c17cec9e80c40a06e669b73903f8f4532c1bc
xinbaobao3775 <48445889+xinbaobao3775@users.noreply.github.com>
2020-05-02 12:53:24 +0800

    Update script.js

--- cn20/script.js
+++ cn20/script.js
@@ -14,6 +14,7 @@ function Judgment (answer,txt,correctAnswer) {
 
 $(".englishButton").click(function(){
     let englishInput = $(".english").val();
+    englishInput = englishInput.toLowerCase();
     Judgment(englishInput,".englishMessage","Monday");
 });
 
